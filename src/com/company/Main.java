a667c1a00ce94b4a1d7f5351e6c8a446758518d2
uabubakirov <abubakirovuran@gmail.com>
2021-06-04 17:13:28 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	String geekTechKb;
+	final int NUM = 0;
+	String word = "Hello World!";
+	geekTechKb = NUM + " " + word;
+        System.out.println(geekTechKb);
+	if (NUM < 0){
+		System.out.println("Вы сохранили отрицательное число");
+	}
+	else if (NUM > 0){
+		System.out.println("Вы сохранили положительное число");
+	}
+	else{
+		System.out.println("Вы сохранили нуль");
+	}
+		Scanner in = new Scanner(System.in);
+		System.out.println("Введите ваше имя: ");
+		String name = in.nextLine();
+		System.out.println("Привет, " + name);
+    }
+}
+
